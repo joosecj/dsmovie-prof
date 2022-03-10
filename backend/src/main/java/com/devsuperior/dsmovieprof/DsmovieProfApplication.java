e57a0c44660b12365193572490aa4ad13aef5d4b
joosecj <josecarloscjj@gmail.com>
2022-03-09 23:10:19 -0300

    Criando projeto

---
+++ backend/src/main/java/com/devsuperior/dsmovieprof/DsmovieProfApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieprof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieProfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieProfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmovieprof/DsmovieProfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieprof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieProfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
